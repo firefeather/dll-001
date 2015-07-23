dcf5c2d04f1d1c819ff93f2411306e845e18fe55
firefeather <755947140@qq.com>
2015-07-23 12:57:16 +0800

    添加命令

--- test.c
+++ test.c
@@ -1,1 +1,7 @@
-https://github.com/firefeather/dll-001.git
+1、申请github账号及建立仓库后；
+2、克隆仓库到本地文件夹：git clone https://github.com/firefeather/dll-001.git
+3、获取更改：git add .
+3、提交更改到缓存区：git commit -m "注释文字^_^"
+4、提交更改到github服务器：git push -u origin master
+5、打开网站查看上传代码！
+	
